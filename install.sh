a1a6c2e8a7ef98fb020f7286b57ea80f365ece12
rphii <raphael.seitz18@gmail.com>
2025-02-16 15:20:15 +0000

    update install

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -ex
 
-if [[ -z $PREFIX ]]; then
-	PREFIX=/
+if [[ -z "${PREFIX}" ]]; then
+    echo "\$PREFIX is empty!" 1>&2
+    exit -1
 fi
 
 # set up
 PROJECT=str
 GROUP=rphii
-DIR_INCLUDES="$PREFIX/usr/include/${GROUP}"
+DIR_INCLUDES="${PREFIX}/include/${GROUP}"
 
 # create system directories
 mkdir -p "${DIR_INCLUDES}" || true
@@ -17,15 +18,15 @@ mkdir -p "${DIR_INCLUDES}" || true
 cp src/*.h "${DIR_INCLUDES}"
 
 #DIR_LIBS_64="lib64"
-if [[ -n LIBS_64 ]]; then
-	DIR_LIBS_64="$PREFIX/$LIBS_64/${GROUP}"
+if [[ -n "${LIBS_64}" ]]; then
+	DIR_LIBS_64="${PREFIX}/${LIBS_64}/${GROUP}"
 	mkdir -p "${DIR_LIBS_64}" || true
 	cp "build/64/${PROJECT}.so" "${DIR_LIBS_64}"
 fi
 
 #DIR_LIBS_32="lib"
-if [[ -n LIBS_32 ]]; then
-	DIR_LIBS_32="$PREFIX/$LIBS_32/${GROUP}"
+if [[ -n "${LIBS_32}" ]]; then
+	DIR_LIBS_32="${PREFIX}/${LIBS_32}/${GROUP}"
 	mkdir -p "${DIR_LIBS_32}" || true
 	cp "build/32/${PROJECT}.so" "${DIR_LIBS_32}"
 fi
